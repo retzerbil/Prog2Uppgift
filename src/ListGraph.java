e799b2f89ff809478cad061f01de96f43518a93e
retzerbil <andreasretzius@gmail.com>
2022-04-25 17:32:50 +0200

    generics start

--- src/ListGraph.java
+++ src/ListGraph.java
@@ -1,6 +1,13 @@
-public class ListGraph {
+import java.util.*;
+
+public class ListGraph<T>{
+
+    T Node;
+
+    Map<T, Set<Edge>> nodes = new HashMap<>();
 
     void add(){
+        nodes.put(Node node);
 
     }
 
