0d293461bc418f96e45689374390d5f873d4ad82
retzerbil <andreasretzius@gmail.com>
2022-04-25 15:38:30 +0200

    Edge methods

--- src/Edge.java
+++ src/Edge.java
@@ -1,2 +1,20 @@
 public class Edge {
+
+    public void getDestination(){
+    }
+
+    public void getWeight(){
+    }
+
+    public void setWeight(){
+    }
+
+    public String getName(){
+        return null;
+    }
+
+    public String toString(){
+        return null;
+    }
+
 }
